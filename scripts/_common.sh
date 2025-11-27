d5395165694cd2b536a29add5073775a24e425bb
Citify <99703172+Citify@users.noreply.github.com>
2025-11-27 16:51:58 -0500

    Create _common.sh

---
+++ scripts/_common.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# Dependencies
+pkg_dependencies="python3-venv python3-pip python3-dev build-essential libxml2-dev libxslt1-dev"
